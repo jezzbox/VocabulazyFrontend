fecb3d716fbf1045b3b657a784a9e52e006f884f
jezzbox <keeltyj@protonmail.com>
2021-04-05 12:34:21 +0100

    added beta warning

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,12 +6,13 @@ const Welcome = () => {
 
 
     return (
-        <article >
+        <article className="flex flex-col items-center">
+          <p className="text-red-500 font-semibold text-2xl p-2">Please Note: This is a beta release (v0.1), you are likely to find bugs and issues. please report these to support@vocabulazy.net.</p>
           <header className="flex justify-center p-6">
             <h1 className="text-center text-5xl">Welcome to VocabuLazy!</h1>
           </header>
           <>
-          <div className="flex justify-center p-6">
+          <div className="flex flex-col items-center p-6">
             <p className="text-xl">VocabuLazy is a flashcard app based on Anki specifically for Spanish vocabulary.</p>
           </div>
           
